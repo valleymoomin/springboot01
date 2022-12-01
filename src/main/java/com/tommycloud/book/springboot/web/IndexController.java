bb226d8f92e1ad702e3a88a2f8e070f3567c25e3
valleymoomin <ktk7167@naver.com>
2022-12-01 17:09:03 +0900

    5-2

---
+++ src/main/java/com/tommycloud/book/springboot/web/IndexController.java
@@ -0,0 +1,34 @@
+package com.tommycloud.book.springboot.web;
+
+import com.tommycloud.book.springboot.service.posts.PostsService;
+import com.tommycloud.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index"; //index.mustache 로 전환 (ViewResolver가 처리)
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
+    }
+}
